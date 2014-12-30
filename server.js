04e6cf55c155a45d374e4c5e677382e3e76c6d8d
rb7373 <abarca.ruben73@gmail.com>
2014-12-30 10:07:56 -0600

    Todos los archivos necesarios

---
+++ server.js
@@ -0,0 +1,72 @@
+var express = require("express");
+var stylus = require('stylus');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var mongoose = require('mongoose');
+
+var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+var app = express();
+
+function compile(str, path) {
+    return stylus(str).set('filename', path);
+};
+
+
+app.set('views', __dirname + '/server/views');
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+
+// parse application/json
+//app.use(bodyParser.json());                        
+
+// parse application/x-www-form-urlencoded
+//app.use(bodyParser.urlencoded({ extended: true }));
+
+// parse multipart/form-data
+//app.use(multer());
+
+app.use(stylus.middleware({
+    src: __dirname + '/public',
+    compile: compile
+}));
+app.use(express.static(__dirname + '/public'));
+
+mongoose.connect('mongodb://admin:fixAdmin@ds063150.mongolab.com:63150/fixdb');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error...'));
+db.once('open', function callback() {
+    console.log('DB is opened');
+});
+
+
+
+//
+var messageSchema = mongoose.Schema({
+    message: String
+});
+var Message = mongoose.model('Message', messageSchema);
+var mongoMessage;
+Message.findOne().exec(function(err, messageDoc) {
+    mongoMessage = messageDoc.message;
+});
+//
+
+
+app.get('/partials/:partialPath', function(req, res) {
+    res.render('partials/' + req.params.partialPath);
+});
+
+app.get('*', function(req, res) {
+    res.render('index', {
+        mongoMessage: mongoMessage
+    });
+});
+
+
+var port = 3030;
+app.listen(port);
+
+console.log('Listening on port: ' + port + ' ...');
